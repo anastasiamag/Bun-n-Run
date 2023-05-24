03013146dcfd29a2c03974edbe1b4bec2c26e116
Anastasia Magopoulou <105965911+anastasiamag@users.noreply.github.com>
2023-05-24 22:40:20 +0300

    updated player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Player : MonoBehaviour
+{
+  [SerializeField] private float moveSpeed = 7f;
+  [SerializeField] public GameInput gameInput;
+  
+
+    private bool isWalking;
+    
+    Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+    Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+    transform.position = transform.position + moveDirection * moveSpeed * Time.deltaTime;
+
+    isWalking = moveDirection != Vector3.zero;
+    float rotateSpeed = 10f;
+    transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+
+}
+
+    public bool IsWalking() {
+        return isWalking;
+    }
+}
+    
+
+//   private void Update(){
+//     Vector2 inputVector = new Vector2(0, 0);
+//     if (Input.GetKey(KeyCode.W)) {
+//             inputVector.y = +1;
+//     } 
+    
+//     if (Input.GetKey(KeyCode.S)) {
+//             inputVector.y = -1;
+//     } 
+    
+//     if (Input.GetKey(KeyCode.A)) {
+//             inputVector.x = -1;
+//     } 
+    
+//     if (Input.GetKey(KeyCode.D)) {
+//             inputVector.x = +1;
+//     } 
+//     inputVector = inputVector.normalized;
