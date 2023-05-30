57022bf9c692e62381e1b5f62055d196236a50a0
Anastasia Magopoulou <105965911+anastasiamag@users.noreply.github.com>
2023-05-30 21:15:44 +0300

    Added Slliced Kitchen Objects

--- Assets/Scripts/Counters/TrashCounter.cs
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrashCounter : MonoBehaviour
+public class TrashCounter : BaseCounter
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override void InteractionCounter(Player player) {
+        if (player.HasKitchenObject()) {
+            player.GetKitchenObject().DestroyKitchenObject();
+        }
     }
 }
