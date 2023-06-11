797ba552df3bac846154a9fc9fc57db7aadeb15d
Anastasia Magopoulou <105965911+anastasiamag@users.noreply.github.com>
2023-06-11 04:12:28 +0300

    Updated README

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -25,7 +25,7 @@ public class GameManager : MonoBehaviour
     private float waitingToStartTimer= 1f;
     private float countdownToStartTimer= 3f;
     private float gamePlayingTimer;
-    private float gamePlayingTimerMax= 30f;
+    private float gamePlayingTimerMax= 40f;
     private bool IsGamePaused = false;
 
 
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Bun 'n' Run
+
+##### _By Anastasia Magopoulou_
+
+```
+The «Bun 'n' Run» was developed for the class "Multimedia Technology"
+```
+
+This game is inspired by "Overcooked 2"
+
+<details>
+<summary> How to play </summary>
+
+![alt text](C:\HowToPlay.png)
+
+</details>
